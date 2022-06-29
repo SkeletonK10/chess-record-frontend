23cc06f685c7cdfbad59ce12e1fb941aebeb98ec
skeletonk10 <poohbilly2@naver.com>
2022-06-30 00:19:44 +0900

    [Feature] Add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import MainPage from './components/MainPage'
 
 const App: React.FC = () => {
   return (
-      <MainPage />
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </Router>
+    
   );
 }
 
