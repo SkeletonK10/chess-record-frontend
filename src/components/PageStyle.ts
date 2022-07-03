48745bb7f75f52d72a33446663d9dcd87caca43a
skeletonk10 <poohbilly2@naver.com>
2022-07-03 21:27:11 +0900

    [Style] Add color to PageStyle

--- src/components/PageStyle.ts
+++ src/components/PageStyle.ts
@@ -1,9 +1,14 @@
 import styled from '@emotion/styled';
 
+import { palette } from '../data';
+
 const PageStyle = styled.div`
   width: 100%;
   height: 100%;
   position: absolute;
+  
+  color: ${palette.wood};
+  background-color: ${palette.ivory};
   
   display: flex;
   flex-direction: column;
