5604e2f246da8d34af1f55968795c948bbcde768
Deokgeun <poohbilly2@naver.com>
2022-07-04 21:09:01 +0900

    [Featrue] Create /game/:id/ route

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import MainPage from './components/Page/MainPage';
+import GameView from './components/Page/GameView';
 import GameList from './components/Page/GameList';
 import GameAdd from './components/Page/GameAdd';
 
@@ -10,6 +11,7 @@ const App: React.FC = () => {
     <Router>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/game/:id/" element={<GameView />} />
         <Route path="/gamelist/" element={<GameList />} />
         <Route path="/newgame/" element={<GameAdd />} />
       </Routes>
--- src/components/Page/GameView/index.tsx
+++ src/components/Page/GameView/index.tsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 import Page from '../../Page';
-import Title from './Title'
+import Title from './Title';
+
+interface BigGameView {
+  id: number;
+  createdAt: Date;
+  white: string;
+  black: string;
+  result: string;
+  notation?: string;
+}
 
 const Comp: React.FC = () => {
+  const { id } = useParams();
+  const rows: Array<BigGameView> = [
+    {
+      id: 6,
+      createdAt: new Date(2021, 11, 6),
+      white: "Magnus",
+      black: "Ian",
+      result: "백 승",
+    },
+    
+    {
+      id: 7,
+      createdAt: new Date(2021, 11, 7),
+      white: "Ian",
+      black: "Magnus",
+      result: "무승부",
+      notation:
+      `1. e4 e5 1/2-1/2`,
+    }
+  ]
   return (
     <Page>
       <Title />
