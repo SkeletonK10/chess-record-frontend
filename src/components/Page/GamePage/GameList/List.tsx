af2ed0a93b728b0c6252578db0a103255ce43444
Deokgeun <poohbilly2@naver.com>
2022-07-04 19:50:25 +0900

    [Feature] Add table content

--- src/components/Page/GamePage/GameList/List.tsx
+++ src/components/Page/GamePage/GameList/List.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from '@emotion/styled';
 
 import { text, palette } from "../../../../data";
@@ -36,12 +37,24 @@ const THStyle = styled.th`
   font-weight: bold;
 `;
 
+const TDStyle = styled.td`
+  border: 3px solid ${palette.wood};
+`;
+
+const LinkButton = styled.div`
+  display: flex;
+  justify-content: center;
+  
+  cursor: pointer;
+`;
+
 const Comp: React.FC = () => {
+  const navigate = useNavigate();
   //const rows = 'GET backend/game/'
   const rows: Array<SmallGameView> = [
     {
       id: 6,
-      createdAt: new Date(2021, 12, 6),
+      createdAt: new Date(2021, 11, 6),
       white: "Carlsen",
       black: "Nepomniachtchi",
       result: "1-0",
@@ -49,13 +62,28 @@ const Comp: React.FC = () => {
     
     {
       id: 7,
-      createdAt: new Date(2021, 12, 7),
+      createdAt: new Date(2021, 11, 7),
       white: "Nepomniachtchi",
       black: "Carlsen",
       result: "1/2-1/2",
     }
   ]
-  let isEmpty = !rows;
+  const isEmpty = !rows;
+  
+  const Comps = rows.map((row: SmallGameView) => {
+    const offset = new Date().getTimezoneOffset() * 60000;
+    const createdAt = new Date(row.createdAt.getTime() - offset);  
+    return (
+      <tr>
+        <TDStyle>{createdAt.toISOString().slice(0, 10)}</TDStyle>
+        <TDStyle>{row.white}</TDStyle>
+        <TDStyle>{row.black}</TDStyle>
+        <TDStyle>{row.result}</TDStyle>
+        <TDStyle><LinkButton onClick={() => navigate(`/game/${row.id}`)}>링크</LinkButton></TDStyle>
+      </tr>
+    );
+  });
+  
   return (
     <ListDivStyle>
       {isEmpty ? (text.gameList.noRecord) : (
@@ -67,6 +95,7 @@ const Comp: React.FC = () => {
             <THStyle>결과</THStyle>
             <THStyle>상세</THStyle>
           </tr>
+          {Comps}
         </TableStyle>
       )}
     </ListDivStyle>
