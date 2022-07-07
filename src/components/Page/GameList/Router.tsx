8023b823ee80ceb9d5a7dafc823c8e43b6f837e2
skeletonk10 <poohbilly2@naver.com>
2022-07-07 16:30:08 +0900

    [Route] Route NotFound for /gamelist/*

--- src/components/Page/GameList/Router.tsx
+++ src/components/Page/GameList/Router.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import GameList from "./index";
+import NotFound from "../NotFound";
 
 const App: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<GameList />} />
+      <Route path="*" element={<NotFound />} />
       <Route path="/:page/" element={<GameList />} />
     </Routes>
     
