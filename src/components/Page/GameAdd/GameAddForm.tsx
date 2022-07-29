1b893dde9fcb8af2748500d6929cbce7035b24e1
skeletonk10 <poohbilly2@naver.com>
2022-07-29 19:48:10 +0900

    [Feature] Add penalty term to gameadd form

--- src/components/Page/GameAdd/GameAddForm.tsx
+++ src/components/Page/GameAdd/GameAddForm.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { SubmitHandler, useForm } from 'react-hook-form';
+import { Controller, SubmitHandler, useForm } from 'react-hook-form';
 import axios from 'axios';
 import styled from '@emotion/styled';
 
-import { text, palette } from "../../../data";
+import { text, palette, penaltyFEN } from "../../../data";
 import { UserInfo, IGameInfo } from "../../../data/types";
 
 const FormStyle = styled.form`
@@ -54,6 +54,13 @@ const TextareaStyle = styled.textarea`
   font-size: 1.2rem;
 `;
 
+const RadioStyle = styled.div`
+  margin-top: 1%;
+  margin-bottom: 1%;
+
+  font-size: 1.2rem;
+`;
+
 const SubmitStyle = styled.input`
   width: 35%;
   height: 10%;
@@ -68,23 +75,25 @@ const SubmitStyle = styled.input`
 
 const Comp: React.FC = () => {
   const [playerList, setPlayerList] = useState([]);
+  const [penalty, setPenalty] = useState("");
+  const penaltyWhite = penaltyFEN.white;
+  const penaltyBlack = penaltyFEN.black;
   
   const loadPlayerList = async () => {
     const response = await axios.get(`${text.backendURL}/playerlist/`);
     setPlayerList(response.data);
   };
-  
   const PlayerOptions = !playerList ?
-  undefined :
-  playerList.map((player: UserInfo, index: number) => {
-    return (
-      <option key={index} value={player.id}>
-        {`${player.name} (${player.rating})`}
-      </option>
-    );
-  });
+    undefined :
+    playerList.map((player: UserInfo, index: number) => {
+      return (
+        <option key={index} value={player.id}>
+          {`${player.name} (${player.rating})`}
+        </option>
+      );
+    });
   
-  const { register, handleSubmit } = useForm<IGameInfo>();
+  const { register, handleSubmit, setValue } = useForm<IGameInfo>();
   const onSubmit: SubmitHandler<IGameInfo> = async (data: IGameInfo) => {
     // API Call: POST backendURL/game/
     try {
@@ -102,7 +111,8 @@ const Comp: React.FC = () => {
   
   useEffect(() => {
     loadPlayerList();
-  });
+    setValue("startpos", penaltyFEN.normal);
+  }, []);
   
   return (
     <FormStyle onSubmit={handleSubmit(onSubmit)}>
@@ -122,6 +132,33 @@ const Comp: React.FC = () => {
         <option value='-1'>선택하지 않음</option>
         {PlayerOptions}
       </SelectStyle>
+      
+      <b>페널티</b>
+      <RadioStyle onChange={(e) => { setValue("startpos", penaltyFEN.normal) }}>
+        <input type="radio" name="startpos" value="0" onChange={(e) => { setPenalty(e.target.value) }} defaultChecked={true} />기본
+        <input type="radio" name="startpos" value="1" onChange={(e) => { setPenalty(e.target.value) }} />백에게 페널티
+        <input type="radio" name="startpos" value="-1" onChange={(e) => { setPenalty(e.target.value) }} />흑에게 페널티
+      </RadioStyle>
+      {penalty !== "1" ? undefined :
+        <SelectStyle {...register("startpos")}>
+          <option value={penaltyWhite.cBishop}>c열 비숍 없이 (-300)</option>
+          <option value={penaltyWhite.fBishop}>f열 비숍 없이 (-300)</option>
+          <option value={penaltyWhite.bKnight}>b열 나이트 없이 (-300)</option>
+          <option value={penaltyWhite.gKnight}>g열 나이트 없이 (-300)</option>
+          <option value={penaltyWhite.aRook}>a열 룩 없이 (-500)</option>
+          <option value={penaltyWhite.hRook}>h열 룩 없이 (-500)</option>
+          <option value={penaltyWhite.queen}>퀸 없이 (-900)</option>
+        </SelectStyle>}
+      {penalty !== "-1" ? undefined :
+        <SelectStyle {...register("startpos")}>
+          <option value={penaltyBlack.cBishop}>c열 비숍 없이 (-300)</option>
+          <option value={penaltyBlack.fBishop}>f열 비숍 없이 (-300)</option>
+          <option value={penaltyBlack.bKnight}>b열 나이트 없이 (-300)</option>
+          <option value={penaltyBlack.gKnight}>g열 나이트 없이 (-300)</option>
+          <option value={penaltyBlack.aRook}>a열 룩 없이 (-500)</option>
+          <option value={penaltyBlack.hRook}>h열 룩 없이 (-500)</option>
+          <option value={penaltyBlack.queen}>퀸 없이 (-900)</option>
+        </SelectStyle>}
       
       <b>결과</b>
       <SelectStyle {...register("result")} defaultValue="-1">
